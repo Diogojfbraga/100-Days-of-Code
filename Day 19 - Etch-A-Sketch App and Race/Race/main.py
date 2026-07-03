ecc0e31e9a766135303bacfcbc2e9002dece9de7
DiogoJFBraga <diogojfbraga@gmail.com>
2026-07-03 18:55:20 +0100

    Day 20

--- Day 19 - Etch-A-Sketch App and Race/Race/main.py
+++ Day 19 - Etch-A-Sketch App and Race/Race/main.py
@@ -30,69 +30,16 @@ while is_race_on:
     for turtle in all_turtles:
         
         if turtle.xcor() > 250:
-            print(turtle.color())
-            # is_race_on = False
+            winner = turtle.pencolor()
+            is_race_on = False
+            if user_choice == winner:
+                print(f"The {winner} turtle won. Your turtle won")
+            else:
+                print(f"The {winner} turtle won. Your turtle lost")
+
+            break
+
         turtle.forward(random.randint(0, 3))
-
-
-
-# while (
-    
-        
-#         turtle.xcor() < 10
-#         or orange.xcor() < 10
-#         or yellow.xcor() < 10
-#         or green.xcor() < 10
-#         or blue.xcor() < 10
-#         or indigo.xcor() < 10
-#         or violet.xcor() < 10
-# ):
-#     red.forward(random.randint(0, 3))
-#     orange.forward(random.randint(0, 3))
-#     yellow.forward(random.randint(0, 3))
-#     green.forward(random.randint(0, 3))
-#     blue.forward(random.randint(0, 3))
-#     indigo.forward(random.randint(0, 3))
-#     violet.forward(random.randint(0, 3))
-
-
-# turtle_coord = []
-
-
-# turtle_coord.append(('red',red.xcor()))
-# turtle_coord.append(('orange',orange.xcor()))
-# turtle_coord.append(('yellow',yellow.xcor()))
-# turtle_coord.append(('green',green.xcor()))
-# turtle_coord.append(('blue',blue.xcor()))
-# turtle_coord.append(('indigo',indigo.xcor()))
-# turtle_coord.append(('violet',violet.xcor()))
-
-# print(turtle_coord)
-
-# coord = 0
-# winner = ""
-
-# for turtle_name, turtle_x in turtle_coord:
-
-#     if turtle_x > coord:
-#         coord = turtle_x
-#         winner = turtle_name
-#         if user_choice == winner:
-#             print("You won")
-#         else:
-#             print(f"{winner} Won, you lose!!")
-
-        
-
-
-
-
-
-
-
-
-
-
 
 
 # Keep the window open until it is clicked
---
+++ Day 20 - Snake Game/main.py
@@ -0,0 +1,90 @@
+# Create the snake body
+
+
+from turtle import Turtle, Screen
+import time, random
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('Black')
+screen.title("My Snake Game")
+screen.tracer(0)
+
+starting_positions = [(0,0), (-20,0), (-40,0)]
+
+squares = []
+
+
+for position in starting_positions:
+    new_square = Turtle(shape='square')
+    new_square.penup()
+
+    new_square.color('gray', 'white')
+    new_square.goto(position)
+    squares.append(new_square)
+
+ # Detect collision with food
+food_x = random.randint(-300,300)
+food_y = random.randint(-300,300)
+
+new_food = Turtle(shape="circle")
+new_food.penup()
+new_food.color("green")
+new_food.shapesize(stretch_len=0.5, stretch_wid=0.5)
+new_food.goto(food_x, food_y)
+
+
+# Move the snake
+game_is_on = True
+
+# Turn the turtle right when D is pressed
+def move_right():
+    squares[0].right(90)
+
+
+# Turn the turtle left when A is pressed
+def move_left():
+    squares[0].left(90)
+
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for square_number in range(len(squares) -1, 0, -1):
+        new_x = squares[square_number - 1].xcor()
+        new_y = squares[square_number - 1].ycor()
+        squares[square_number].goto(new_x, new_y)
+
+    squares[0].forward(10)
+
+    
+    
+       
+
+    
+
+    # Control the snake
+    screen.listen()  
+    screen.onkeypress(key="d", fun=move_right)
+    screen.onkeypress(key="a", fun=move_left)
+
+
+
+        
+if food_x == squares[0].xcor():
+        print("hit")
+    
+
+
+screen.listen()
+# screen.onkey(key="a", fun=move_left)
+
+
+
+# Create a scoreboard
+# Detect collision with wall
+# Detect colision with tail
+
+
+screen.exitonclick()
