c113fcd689d3d7b613065bf864b53a9dda58f7e9
DiogoJFBraga <diogojfbraga@gmail.com>
2026-07-04 16:27:10 +0100

    Snake

--- Day 20 - Snake Game/main.py
+++ Day 20 - Snake Game/main.py
@@ -14,6 +14,11 @@ starting_positions = [(0,0), (-20,0), (-40,0)]
 
 squares = []
 
+scoreboard = Turtle()
+
+
+
+
 
 for position in starting_positions:
     new_square = Turtle(shape='square')
@@ -23,16 +28,27 @@ for position in starting_positions:
     new_square.goto(position)
     squares.append(new_square)
 
- # Detect collision with food
-food_x = random.randint(-300,300)
-food_y = random.randint(-300,300)
 
-new_food = Turtle(shape="circle")
-new_food.penup()
-new_food.color("green")
-new_food.shapesize(stretch_len=0.5, stretch_wid=0.5)
-new_food.goto(food_x, food_y)
 
+food_location = []
+
+def food():
+
+    new_food = Turtle(shape="circle")
+    new_food.penup()
+    new_food.color("green")
+    new_food.shapesize(stretch_len=0.5, stretch_wid=0.5)
+
+
+     # Detect collision with food
+    food_x = random.randint(-300,300)
+    food_y = random.randint(-300,300)
+
+    new_food.goto(food_x, food_y)
+
+    return new_food
+
+current_food = food()
 
 # Move the snake
 game_is_on = True
@@ -46,10 +62,12 @@ def move_right():
 def move_left():
     squares[0].left(90)
 
+score = 0
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+
 
     for square_number in range(len(squares) -1, 0, -1):
         new_x = squares[square_number - 1].xcor()
@@ -58,22 +76,37 @@ while game_is_on:
 
     squares[0].forward(10)
 
-    
-    
-       
-
-    
-
-    # Control the snake
+        # Control the snake
     screen.listen()  
     screen.onkeypress(key="d", fun=move_right)
     screen.onkeypress(key="a", fun=move_left)
+    
+    
+    if squares[0].distance(current_food) < 10:
+        scoreboard.clear()
+        score += 1
 
-
+        current_food.hideturtle()
+        current_food = food()
 
         
-if food_x == squares[0].xcor():
-        print("hit")
+        
+    scoreboard.color("white")
+    scoreboard.penup()
+    scoreboard.goto(0,250)
+    scoreboard.write(f"Score: {score}", move=False, align="center", font=("Arial", 18, "normal"))
+    scoreboard.hideturtle()     
+
+    
+
+
+
+
+    # print(squares[0].xcor())
+    # print(food_x)
+    # print(squares[0].distance(food_x, food_y))   
+
+
     
 
 
