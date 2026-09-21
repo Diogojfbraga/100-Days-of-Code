25a9212c8ad79a9047a0b67f8628dcf4fcbc06e3
DiogoJFBraga <diogojfbraga@gmail.com>
2026-09-21 17:20:49 +0100

    Day 4

--- Day 34 - Trivia API/quizzler-app-start/main.py
+++ Day 34 - Trivia API/quizzler-app-start/main.py
@@ -1,10 +1,21 @@
 from question_model import Question
-from data import question_data
+# from data import question_data
 from quiz_brain import QuizBrain
+import requests
+
+response = requests.get(url="https://opentdb.com/api.php?amount=10")
+response.raise_for_status()
+
+data = response.json()
+
+test = data["results"][1]["question"]
+
+print(test)
+
 
 question_bank = []
-for question in question_data:
-    question_text = question["question"]
+for question in data["results"]:
+    question_text =  question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
